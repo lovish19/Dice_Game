cd37c66d81b1f031e5f8a48c770dceac91e8aedf
LOVISH SINGLA <singlalovish36@gmail.com>
2023-07-14 17:29:26 +0530

    index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var randomvalue=Math.floor(Math.random()*6)+1;
- var randomImage="images/dice"+randomvalue+".png";
- var img1=document.querySelectorAll("img")[0];
- img1.setAttribute("src", randomImage);
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomDiceImage= "dice" + randomNumber1 + ".png"; // concatenation to create that image (  dice1.png - dice6.png   )
+var randomImageSource= "/images/images/" + randomDiceImage; //this will generate source    
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
 
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImageSource2= "/images/images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
- var randomvalue1=Math.floor(Math.random()*6)+1;
- var randomImage1="images/dice"+randomvalue1+".png";
- var img2=document.querySelectorAll("img")[1];
- img2.setAttribute("src", randomImage1);
-
-
- if(randomvalue>randomvalue1)
- document.querySelector("h1").innerHTML="Player1 Won";
- else if(randomvalue<randomvalue1)
- document.querySelector("h1").innerHTML="Player2 Won";
- else
- document.querySelector("h1").innerHTML="Draw";
+if (randomNumber1 >  randomNumber2) {
+    document.querySelector("h1").innerHTML=" 🚩 Player 1 Wins!";
+}
+else if(randomNumber1 <  randomNumber2){
+    document.querySelector("h1").innerHTML=" 🚩 Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML=" 🏳️ It's A Draw!";
+}
